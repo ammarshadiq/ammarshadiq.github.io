60c57427e2e71d20689cd22767b6557b12054cc3
ammarshadiq <ammar.shadiq@gmail.com>
2017-02-12 20:04:21 +0100

    First commit to github.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/blaugh/"
+http_images_path = "/blaugh/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/blaugh/fonts"
+css_dir = "public/blaugh/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
