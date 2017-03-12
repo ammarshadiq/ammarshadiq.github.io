22d2d7cecfe864d0ed60b840f73f7ec10f4313e8
ammarshadiq <ammar.shadiq@gmail.com>
2017-03-12 21:03:35 +0100

    Site updated at 2017-03-12 20:03:35 UTC

---
+++ planetaise/obj/cassini/cassini.js
@@ -0,0 +1,147 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "sourceFile"    : "cassini_lowres17.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 16078,
+        "faces"         : 15707,
+        "normals"       : 0,
+        "uvs"           : 4058,
+        "materials"     : 10
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "whitedish",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.940377, 0.940377, 0.940377],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "red",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.683972, 0.007422, 0.0],
+	"colorSpecular" : [0.04902, 0.045098, 0.05098],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "vdark",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.075698, 0.075698, 0.075698],
+	"colorSpecular" : [0.05098, 0.04902, 0.056863],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "gold2",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.577255, 0.449255, 0.00753],
+	"colorSpecular" : [0.170588, 0.109804, 0.092157],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "gold",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.552157, 0.469334, 0.190745],
+	"colorSpecular" : [0.135294, 0.064706, 0.056863],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "_--_default_--",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.532078, 0.532078, 0.532078],
+	"colorSpecular" : [0.052941, 0.052941, 0.052941],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "darkishred",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.529569, 0.0, 0.012549],
+	"colorSpecular" : [0.056863, 0.056863, 0.056863],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 11353132,
+	"DbgIndex" : 7,
+	"DbgName" : "metallic",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.444235, 0.444235, 0.444235],
+	"colorSpecular" : [0.137255, 0.137255, 0.137255],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 3744854,
+	"DbgIndex" : 8,
+	"DbgName" : "black",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.016302, 0.016302, 0.016302],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 12355935,
+	"DbgIndex" : 9,
+	"DbgName" : "orange",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.23042, 0.0],
+	"colorSpecular" : [0.052941, 0.052941, 0.052941],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 92.156863,
+	"transparency" : 1.0
+	}],
+
+    "buffers": "cassini.bin"
+
+}
