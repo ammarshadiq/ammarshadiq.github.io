3e3cc1dc5f493b8a281b7b8269c41f9ae14ea173
ammarshadiq <ammar.shadiq@gmail.com>
2017-03-12 21:53:19 +0100

    config

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/blaugh/"
-http_images_path = "/blaugh/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/blaugh/fonts"
-css_dir = "public/blaugh/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
