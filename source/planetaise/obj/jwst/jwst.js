9c22d5b0b4ccb3a6d33e55553b836d294a5543aa
ammarshadiq <ammar.shadiq@gmail.com>
2018-04-09 23:05:41 +0200

    change front page contents

---
+++ source/planetaise/obj/jwst/jwst.js
@@ -0,0 +1,615 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "jwt.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 201733,
+        "faces"         : 192641,
+        "normals"       : 198462,
+        "uvs"           : 203460,
+        "materials"     : 46
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "JWST_a1:prop_alumSG",
+	"colorAmbient" : [0.09, 0.11, 0.13],
+	"colorDiffuse" : [0.21, 0.26, 0.3],
+	"colorSpecular" : [0.76, 0.83, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "JWST_a1:silver_mliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.12, 0.1, 0.15],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "JWST_a1:asm_covrcleats_hr_car_v001:mboom_alumSG",
+	"colorAmbient" : [0.1, 0.1, 0.1],
+	"colorDiffuse" : [0.17, 0.23, 0.3],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "JWST_a1:mboom_sml_pinkmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15658496,
+	"DbgIndex" : 4,
+	"DbgName" : "JWST_a1:pinkmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 61166,
+	"DbgIndex" : 5,
+	"DbgName" : "JWST_a1:blinn2SG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.14, 0.14, 0.14],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15597806,
+	"DbgIndex" : 6,
+	"DbgName" : "JWST_a1:tower_blkmli_tubesSG",
+	"colorAmbient" : [0.1, 0.1, 0.09],
+	"colorDiffuse" : [0.13, 0.12, 0.11],
+	"colorSpecular" : [1.0, 1.0, 0.95],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 10665014,
+	"DbgIndex" : 7,
+	"DbgName" : "JWST_a1:blk_mliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.11, 0.1, 0.13],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 5292423,
+	"DbgIndex" : 8,
+	"DbgName" : "JWST_a1:sc_rad_smliSG1",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.09, 0.08, 0.1],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 7698256,
+	"DbgIndex" : 9,
+	"DbgName" : "JWST_a1:prop_drkalumSG",
+	"colorAmbient" : [0.07, 0.09, 0.1],
+	"colorDiffuse" : [0.14, 0.17, 0.2],
+	"colorSpecular" : [0.6, 0.65, 0.7],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 5279418,
+	"DbgIndex" : 10,
+	"DbgName" : "JWST_a1:black0_SG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 9686361,
+	"DbgIndex" : 11,
+	"DbgName" : "JWST_a1:bipod_post_pmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 6637601,
+	"DbgIndex" : 12,
+	"DbgName" : "JWST_a1:mboom_boom_pinkmliSG1",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 6210605,
+	"DbgIndex" : 13,
+	"DbgName" : "JWST_a1:sc_panels_pmliSG1",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 2741438,
+	"DbgIndex" : 14,
+	"DbgName" : "JWST_a1:sc_post_pmliSG1",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 3539704,
+	"DbgIndex" : 15,
+	"DbgName" : "JWST_a1:sc_sml_pmliSG1",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 4855124,
+	"DbgIndex" : 16,
+	"DbgName" : "initialShadingGroup",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.5, 0.5, 0.5],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 4491946,
+	"DbgIndex" : 17,
+	"DbgName" : "JWST_a1:mboom_alumSG",
+	"colorAmbient" : [0.1, 0.1, 0.1],
+	"colorDiffuse" : [0.17, 0.23, 0.3],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 1694522,
+	"DbgIndex" : 18,
+	"DbgName" : "JWST_a1:mboom_sml_pinkmliSG1",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 3905582,
+	"DbgIndex" : 19,
+	"DbgName" : "JWST_a1:mboom_boom_pinkmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 12232717,
+	"DbgIndex" : 20,
+	"DbgName" : "JWST_a1:shld_silv1SG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.08, 0.08, 0.08],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 10219521,
+	"DbgIndex" : 21,
+	"DbgName" : "JWST_a1:linn3SG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.14, 0.14, 0.14],
+	"colorSpecular" : [0.53, 0.53, 0.53],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 3514944,
+	"DbgIndex" : 22,
+	"DbgName" : "JWST_a1:sc_post_pmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 6120867,
+	"DbgIndex" : 23,
+	"DbgName" : "JWST_a1:straylgt_blkmliSG",
+	"colorAmbient" : [0.1, 0.1, 0.09],
+	"colorDiffuse" : [0.13, 0.12, 0.11],
+	"colorSpecular" : [1.0, 1.0, 0.95],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 14140707,
+	"DbgIndex" : 24,
+	"DbgName" : "JWST_a1:iec_pnkmli_smlSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 3842788,
+	"DbgIndex" : 25,
+	"DbgName" : "JWST_a1:omnimirror_blinnSG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.61, 0.61, 0.61],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 10737413,
+	"DbgIndex" : 26,
+	"DbgName" : "JWST_a1:mboon_silvermliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.12, 0.1, 0.15],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15819399,
+	"DbgIndex" : 27,
+	"DbgName" : "JWST_a1:asm_covrcleats_hr_car_v001:twr_blkpaintSG",
+	"colorAmbient" : [0.1, 0.1, 0.1],
+	"colorDiffuse" : [0.2, 0.2, 0.2],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 538552,
+	"DbgIndex" : 28,
+	"DbgName" : "JWST_a1:twrblkmliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.11, 0.1, 0.13],
+	"colorSpecular" : [1.0, 1.0, 0.95],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 10872228,
+	"DbgIndex" : 29,
+	"DbgName" : "JWST_a1:mboom_blkmliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.11, 0.1, 0.13],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 2871229,
+	"DbgIndex" : 30,
+	"DbgName" : "JWST_a1:sacellsSG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.92, 0.86, 1.0],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 10996435,
+	"DbgIndex" : 31,
+	"DbgName" : "JWST_a1:sc_rad_smliSG",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.09, 0.08, 0.1],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 4479144,
+	"DbgIndex" : 32,
+	"DbgName" : "JWST_a1:sa_edge_goldSG",
+	"colorAmbient" : [0.1, 0.09, 0.06],
+	"colorDiffuse" : [0.15, 0.14, 0.08],
+	"colorSpecular" : [1.0, 0.97, 0.83],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15714455,
+	"DbgIndex" : 33,
+	"DbgName" : "JWST_a1:mirror_blinnSG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.77, 0.63, 0.06],
+	"colorSpecular" : [1.0, 0.99, 0.51],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 13019117,
+	"DbgIndex" : 34,
+	"DbgName" : "JWST_a1:whitepaintSG",
+	"colorAmbient" : [0.41, 0.41, 0.41],
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 16601446,
+	"DbgIndex" : 35,
+	"DbgName" : "JWST_a1:asm_corecovrs_hr_car_v002:corecov_smliSG",
+	"colorAmbient" : [0.11, 0.1, 0.13],
+	"colorDiffuse" : [0.09, 0.08, 0.1],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 15343561,
+	"DbgIndex" : 36,
+	"DbgName" : "JWST_a1:bafl_graphiteSG",
+	"colorAmbient" : [0.1, 0.1, 0.09],
+	"colorDiffuse" : [0.15, 0.15, 0.14],
+	"colorSpecular" : [0.9, 0.89, 0.81],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 9344065,
+	"DbgIndex" : 37,
+	"DbgName" : "JWST_a1:sc_ring_ssmSG",
+	"colorAmbient" : [0.08, 0.1, 0.15],
+	"colorDiffuse" : [0.1, 0.13, 0.2],
+	"colorSpecular" : [0.79, 0.84, 0.96],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 14703008,
+	"DbgIndex" : 38,
+	"DbgName" : "JWST_a1:sc_panels_pmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 3823498,
+	"DbgIndex" : 39,
+	"DbgName" : "JWST_a1:bafl_black0SG",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.0, 0.0, 0.0],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 11820753,
+	"DbgIndex" : 40,
+	"DbgName" : "JWST_a1:blackpaintSG",
+	"colorAmbient" : [0.1, 0.1, 0.1],
+	"colorDiffuse" : [0.2, 0.2, 0.2],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 4663622,
+	"DbgIndex" : 41,
+	"DbgName" : "JWST_a1:silver_mliSG1",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.12, 0.1, 0.15],
+	"colorSpecular" : [0.87, 0.85, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 768805,
+	"DbgIndex" : 42,
+	"DbgName" : "JWST_a1:blk_mliSG1",
+	"colorAmbient" : [0.04, 0.04, 0.05],
+	"colorDiffuse" : [0.11, 0.1, 0.13],
+	"colorSpecular" : [0.9, 0.9, 0.9],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 1338687,
+	"DbgIndex" : 43,
+	"DbgName" : "JWST_a1:shld_pink1SG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 11485920,
+	"DbgIndex" : 44,
+	"DbgName" : "JWST_a1:sc_sml_pmliSG",
+	"colorAmbient" : [0.1, 0.07, 0.08],
+	"colorDiffuse" : [0.15, 0.1, 0.12],
+	"colorSpecular" : [0.9, 0.85, 0.87],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	},
+
+	{
+	"DbgColor" : 6366205,
+	"DbgIndex" : 45,
+	"DbgName" : "JWST_a1:sa_edge_goldSG1",
+	"colorAmbient" : [0.1, 0.09, 0.06],
+	"colorDiffuse" : [0.15, 0.14, 0.08],
+	"colorSpecular" : [1.0, 0.97, 0.83],
+	"illumination" : 6,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.0
+	}],
+
+    "buffers": "jwst.bin"
+
+}
